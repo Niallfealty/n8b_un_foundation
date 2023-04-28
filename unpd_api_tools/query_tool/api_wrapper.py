acf6569ef5fda7c12e997c4be2cdefd5016aef71
niallfealty <niallfealty>
2023-04-28 02:10:06 -0700

    adapted example download script into functions to load paginated data from an endpoint into a dataframe

--- unpd_api_tools/query_tool/api_wrapper.py
+++ unpd_api_tools/query_tool/api_wrapper.py
@@ -7,7 +7,8 @@ import pandas as pd
 
 CONFIG_PATH = "config.json"
 
-config = json.load(CONFIG_PATH)
+with open(CONFIG_PATH, "r") as fp:
+    config = json.load(fp)
 
 # Loads the base url for calling the API
 base_url = config["api_url"]
@@ -17,48 +18,32 @@ base_url = config["api_url"]
 target = base_url + "/indicators/"
 
 def get_next_batch(target):
-    response = requests.get(target)
+    ''' Grabs a batch of data
+        
+        @target is the url to use for this batch/page
+
+        returns the url for the next page and a json_normalized dataframe of response
+    '''
+    response = get(target)
     j = response.json()
     return j['nextPage'], pd.json_normalize(j['data'])
 
 def get_data_page_iterator(target):
-    while target != None:
+    ''' Takes the url @target and returns an iterator which iterates
+        over pages of data from this endpoint.
+
+        @target is the url for the endpoint to download from
+
+        returns a dataframe of the data from the response
+    '''
+    while target is not None:
         #call the API for the next page
         target, df = get_next_batch(target)
         yield df
 
 def pull_paginated_data_to_df(target_url):
     ''' pull all paged data into a dataframe '''
-    initial_response = get(target_url)
-    return pd.concat(get_data_page_iterator(initial_response))
+    return pd.concat(get_data_page_iterator(target_url))
 
-print(pull_paginated_data_to_df(target))
-
-import sys
-sys.exit()
-
-# Get the response, which includes the first page of data as well as information on pagination and number of records
-response = requests.get(target)
-
-# Converts call into JSON
-j = response.json()
-
-# Converts JSON into a pandas DataFrame.
-df = pd.json_normalize(j['data']) # pd.json_normalize flattens the JSON to accomodate nested lists within the JSON structure
-
-# Loop until there are new pages with data
-while j['nextPage'] != None:
-    # Reset the target to the next page
-    target = j['nextPage']
-
-    #call the API for the next page
-    response = requests.get(target)
-
-    # Convert response to JSON format
-    j = response.json()
-
-    # Store the next page in a data frame
-    df_temp = pd.json_normalize(j['data'])
-
-    # Append next page to the data frame
-    df = df.append(df_temp)
+if __name__ == "__main__":
+    print(pull_paginated_data_to_df(target))
