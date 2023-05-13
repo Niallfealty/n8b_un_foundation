74f6f3509a887098d5ba19ae4a46d55f3a58d65b
Niallfealty <niallfealty@gmail.com>
2023-05-13 06:55:37 -0700

    Wrote code for finding data

--- model_analysis/exploratory/src/data_load.py
+++ model_analysis/exploratory/src/data_load.py
@@ -4,6 +4,9 @@
     (will add a config later)
 """
 
+from os import listdir
+from os.path import isfile, isdir, join
+
 class DataLoader:
     """ Loader for loading data from multiple different sources
     """
@@ -20,3 +23,24 @@ class DataLoader:
     def _load_gbd(self):
         ''' Parse the file contents and load selected data
         '''
+        return
+
+    def _parse_gbd_folder(self):
+        ''' List files in the gbd data folder and bucket it them
+            for loading the data
+        '''
+        data_folder = self._find_data(data_folder_root)
+        filenames = [filename.lower() for ]
+    
+    def _find_data(self, data_root):
+        ''' Work down file path to find at least one csv
+        '''
+        contained_files = listdir(data_root)
+        if len(contained_files) == 1 and isdir(contained_files[0]):
+            return self._find_data(join(data_root, contained_files[0]))
+        elif any([p.lower().endswith(".csv") for p in contained_files]):
+            return data_root
+        elif len([d for d in contained files if (isdir(d) and self._find_data(d) is not None)]):
+            return [d for d in contained files if (isdir(d) and self._find_data(d) is not None)][0]
+        else:
+            return None
