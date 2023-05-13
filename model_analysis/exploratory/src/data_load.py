05ab32d24ca08f13bdf79d237d8fca3d8e406be0
Niallfealty <niallfealty@gmail.com>
2023-05-13 07:20:41 -0700

    Added path parser and some debugging

--- model_analysis/exploratory/src/data_load.py
+++ model_analysis/exploratory/src/data_load.py
@@ -6,19 +6,22 @@
 
 from os import listdir
 from os.path import isfile, isdir, join
+from re import compile as re_compile
 
 class DataLoader:
     """ Loader for loading data from multiple different sources
     """
+    FORECAST_NAME = re_compile(r"ihme_pop_\d{4}_\d{4}")
+    END_CODE = re_compile(r"_([a-z0-9]*).csv$|_([a-z0-9]*)$")
 
     def __init__(self,
             data_folder_root,
-            unpd=False
+            unpd=False,
             gbd=True,
             earth4all=False):
 
         self.data_folder_root = data_folder_root
-        self.unpd = unpd, self.gbd = gbd, self.earth4all = earth4all
+        self.unpd, self.gbd, self.earth4all = unpd, gbd, earth4all
 
     def _load_gbd(self):
         ''' Parse the file contents and load selected data
@@ -29,8 +32,8 @@ class DataLoader:
         ''' List files in the gbd data folder and bucket it them
             for loading the data
         '''
-        data_folder = self._find_data(data_folder_root)
-        filenames = [filename.lower() for ]
+        data_folder = self._find_data(self.data_folder_root)
+        filenames = {".".join(filename.lower().split(".")[:-1]): join(data_folder, filename) for filename in listdir(data_folder)}
     
     def _find_data(self, data_root):
         ''' Work down file path to find at least one csv
@@ -40,7 +43,20 @@ class DataLoader:
             return self._find_data(join(data_root, contained_files[0]))
         elif any([p.lower().endswith(".csv") for p in contained_files]):
             return data_root
-        elif len([d for d in contained files if (isdir(d) and self._find_data(d) is not None)]):
-            return [d for d in contained files if (isdir(d) and self._find_data(d) is not None)][0]
+        elif len([d for d in contained_files if (isdir(d) and self._find_data(d) is not None)]):
+            return [d for d in contained_files if (isdir(d) and self._find_data(d) is not None)][0]
         else:
             return None
+
+    def _parse_naming_convention(self, fname):
+        fname_no_ext = fname.split(".")[0] # assume no other '.'s
+        forecast_name = FORECAST_NAME.findall(fname)[0]
+        end_code = END_CODE.findall(fname)[0]
+        scenario = fname_no_ext[len(forecast_name)+1:-(len(end_code)+1)]
+        return forecast_name, scenario, end_code
+
+    def _read_data(self, filepath, scenario, forecast_name):
+        df = read_csv(filepath)
+        df["scenario"] = scenario
+        df["forecast_name"] = forecast_name
+        return df
