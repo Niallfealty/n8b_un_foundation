61bafe2216f46a43322d2503ebe9f46d3666330f
Niallfealty <niallfealty@gmail.com>
2023-05-14 06:55:44 -0700

    Code for working out when next 8Bn reached and some extrapolation plots

---
+++ model_analysis/exploratory/src/cumulative_stats.py
@@ -0,0 +1,15 @@
+""" Some tools for computing cumulative stats on data
+"""
+
+def find_births_threshold(df,
+        births_col="BirthsSingle",
+        time_col="Time",
+        scenario_col="Variant",
+        max_time=200):
+    ''' Function to get out the births threshold (in this case to find
+        the year in which the last of the next 8Bn are born)
+    '''
+    df\
+        .sort_values([scenario_col, time_col], ascending=True)
+        .groupby(scenario_col)\
+        .rolling(max_time, center=False, on=time_col)
